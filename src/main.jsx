43333ba2e0b652d6d072d1155901340c8df6934e
batutemizer <95549034+batutemizer@users.noreply.github.com>
2025-07-24 19:01:23 +0300

    Sayfa geçiş animasyonu kaldırıldı, klasik route yapısına dönüldü. Hata giderildi.

--- src/main.jsx
+++ src/main.jsx
@@ -2,30 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AdminLogin from './pages/AdminLogin'
 import AdminPanel from './pages/AdminPanel'
-import { CSSTransition, TransitionGroup } from 'react-transition-group'
-
-function AnimatedRoutes() {
-  const location = useLocation();
-  return (
-    <TransitionGroup>
-      <CSSTransition key={location.pathname} classNames="page" timeout={400}>
-        <Routes location={location}>
-          <Route path="/" element={<App />} />
-          <Route path="/admin-login" element={<AdminLogin />} />
-          <Route path="/admin" element={<AdminPanel />} />
-        </Routes>
-      </CSSTransition>
-    </TransitionGroup>
-  );
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AnimatedRoutes />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/admin" element={<AdminPanel />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 )
